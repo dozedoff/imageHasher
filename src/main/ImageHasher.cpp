7fa6828571d96ea4e9126042f393431d47cea730
dozedoff <dozedoffagain@gmail.com>
2013-08-13 14:08:13 +0200

    Project setup

---
+++ src/main/ImageHasher.cpp
@@ -0,0 +1,17 @@
+/*
+ * imageHasher.cpp
+ *
+ *  Created on: 13 Aug 2013
+ *      Author: nicholas
+ */
+
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/configurator.h>
+
+using namespace log4cplus;
+
+int main(int argc, char **argv) {
+	 BasicConfigurator config;
+	 config.configure();
+}
