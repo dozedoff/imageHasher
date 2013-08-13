b96573a116c08a24fe897461f10956fe9ee25ff2
dozedoff <dozedoffagain@gmail.com>
2013-08-13 18:29:16 +0200

    Search path handling done via Boost-Filesystem

--- src/main/ImageHasher.cpp
+++ src/main/ImageHasher.cpp
@@ -9,9 +9,34 @@
 #include <log4cplus/loggingmacros.h>
 #include <log4cplus/configurator.h>
 
-using namespace log4cplus;
+#include <boost/filesystem.hpp>
 
-int main(int argc, char **argv) {
+using namespace log4cplus;
+using namespace boost::filesystem;
+
+Logger logger;
+
+// Prototypes
+path getPath(char**);
+
+int main(int argc, char* argv[]) {
 	 BasicConfigurator config;
 	 config.configure();
+	 logger = Logger::getInstance(LOG4CPLUS_TEXT("ImageHasher"));
+
+	 path path = getPath(argv);
+}
+
+path getPath(char* argv[]) {
+	 path path;
+
+	 if(NULL == argv[1]) {
+		 path = boost::filesystem::current_path();
+		 LOG4CPLUS_INFO(logger, "No search path given, using current directory " << path);
+	 }else{
+		path = boost::filesystem::path(argv[1]);
+		LOG4CPLUS_INFO(logger, "Search path is " << path);
+	 }
+
+	return path;
 }
