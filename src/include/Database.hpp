2cdd37587eba53c2fcd106dfe6f1a395bbbf5c13
dozedoff <dozedoffagain@gmail.com>
2013-08-22 20:55:50 +0200

    Added databasse function prototypes

--- src/include/Database.hpp
+++ src/include/Database.hpp
@@ -9,20 +9,28 @@
 #define DATABASE_HPP_
 
 #include <sqlite3.h>
+#include <boost/filesystem.hpp>
 
 class Database {
 public:
 	Database();
-
 	bool init();
+
+	typedef struct db_dat {
+		boost::filesystem::path filePath;
+		uint64_t pHash;
+	};
 private:
 	bool initOk;
 	sqlite3 *db;
 	char* errMsg;
 
 	void db_exec(char*);
-	static int callback(void*, int, char**, char**);
+	bool db_add(db_dat);
+	void db_drain(std::list<db_dat>&);
+	void doWork();
 
+	static int callback(void*, int, char**, char**);
 };
 
 #endif /* DATABASE_HPP_ */
