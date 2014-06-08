580c17fb1148d48f97ef764d450b66ea2ae78a7f
dozedoff <dozedoffagain@gmail.com>
2014-06-08 02:09:23 +0200

    Schema upgraded to version 2
    Hashes are moved own table.
    Normalization and possible faster hashing.

--- src/main/Database.cpp
+++ src/main/Database.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "../include/Database.hpp"
+#include <string>
 
 const char *dbName = "imageHasher.db";
 
@@ -23,7 +24,7 @@ const char *getSHAQuery = "SELECT `sha256` FROM `imagerecord` WHERE `path` = ?;"
 const char *startTransactionQuery = "BEGIN TRANSACTION;";
 const char *commitTransactionQuery = "COMMIT TRANSACTION;";
 
-static const int CURRENT_DB_SCHEMA_VERSION = 1;
+static const int CURRENT_DB_SCHEMA_VERSION = 2;
 
 Database::Database(const char* dbPath) {
 	dbName = dbPath;
@@ -111,17 +112,71 @@ void Database::updateSchema() {
 
 	if(dbVersion == CURRENT_DB_SCHEMA_VERSION){
 		LOG4CPLUS_INFO_STR(logger,"DB schema is up to date.");
+		return;
 	}else if(dbVersion > CURRENT_DB_SCHEMA_VERSION) {
-		LOG4CPLUS_INFO(logger,"DB schema is newer than the current schema, aborting...");
+		LOG4CPLUS_ERROR(logger,"DB schema is newer than the current schema, aborting...");
 		throw std::runtime_error("DB schema is newer than the current version");
 	}else{
 		LOG4CPLUS_INFO(logger,"DB schema is out of date, actual: " << dbVersion << " current: " << CURRENT_DB_SCHEMA_VERSION << " updating...");
+
 	switch (dbVersion) {
 		case 0:
 			exec(const_cast<char *>("ALTER TABLE imagerecord ADD COLUMN `sha256` VARCHAR NOT NULL DEFAULT ''"));
+		case 1:
+			// create hash table
+			exec(const_cast<char *>("CREATE TABLE IF NOT EXISTS `hash` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `sha256` VARCHAR NOT NULL DEFAULT '', `pHash` VARCHAR NOT NULL, PRIMARY KEY (`id`));"));
+			exec(const_cast<char *>("CREATE UNIQUE INDEX IF NOT EXISTS `hash_sha_index` ON `hash` (`sha256`);"));
+			exec(const_cast<char *>("CREATE INDEX IF NOT EXISTS `hash_phash_index` ON `hash` (`pHash`);"));
+
+			// add the default record
+			exec(const_cast<char *>("INSERT OR IGNORE INTO `hash` (`id`,`sha256`,`pHash`) VALUES (0,'','');"));
+
+			// add ID column to imagerecord
+			exec(const_cast<char *>("ALTER TABLE `imagerecord` ADD COLUMN `hash_id` INTEGER NOT NULL DEFAULT 0"));
+
+			// add ID column to filterrecord
+			exec(const_cast<char *>("ALTER TABLE `filterrecord` ADD COLUMN `hash_id` INTEGER NOT NULL DEFAULT 0"));
+
+			// populate hash table with existing hash data
+			exec(const_cast<char *>("INSERT INTO `hash` (sha256,pHash) SELECT DISTINCT `sha256`,`pHash` FROM `imagerecord`;"));
+
+			// link tables by ID
+			exec(const_cast<char *>("UPDATE imagerecord SET hash_id = (SELECT id FROM hash WHERE imagerecord.sha256 = hash.sha256);"));
+			exec(const_cast<char *>("UPDATE filterrecord SET hash_id = (SELECT id FROM hash WHERE filterrecord.pHash = hash.pHash);"));
+
+			// create temp tables
+			exec(const_cast<char *>("CREATE TEMP TABLE `imagerecord_new` (`path` VARCHAR NOT NULL , `hash_id` INTEGER NOT NULL DEFAULT 0  , PRIMARY KEY (`path`) );"));
+			exec(const_cast<char *>("CREATE TEMP TABLE `filterrecord_new` (`hash_id` INTEGER NOT NULL DEFAULT 0, `reason` VARCHAR NOT NULL, PRIMARY KEY (`hash_id`) );"));
+
+			// copy data
+			exec(const_cast<char *>("INSERT INTO `imagerecord_new` (`path`,`hash_id`) SELECT `path`,`hash_id` FROM `imagerecord`;"));
+			exec(const_cast<char *>("INSERT INTO `filterrecord_new` (`hash_id`,`reason`) SELECT `hash_id`,`reason` FROM `filterrecord`;"));
+
+			// drop old tables
+			exec(const_cast<char *>("DROP TABLE `imagerecord`;"));
+			exec(const_cast<char *>("DROP TABLE `filterrecord`;"));
+
+			// create new tables
+			exec(const_cast<char *>("CREATE TABLE `imagerecord` (`path` VARCHAR NOT NULL , `hash_id` INTEGER NOT NULL DEFAULT 0  , PRIMARY KEY (`path`) );"));
+			exec(const_cast<char *>("CREATE TABLE `filterrecord` (`hash_id` INTEGER NOT NULL DEFAULT 0, `reason` VARCHAR NOT NULL, PRIMARY KEY (`hash_id`) );"));
+
+			// restore data
+			exec(const_cast<char *>("INSERT INTO `imagerecord` (`path`,`hash_id`) SELECT `path`,`hash_id` FROM `imagerecord_new`;"));
+			exec(const_cast<char *>("INSERT INTO `filterrecord` (`hash_id`,`reason`) SELECT `hash_id`,`reason` FROM `filterrecord_new`;"));
+
+			// drop temp tables
+			exec(const_cast<char *>("DROP TABLE `imagerecord_new`;"));
+			exec(const_cast<char *>("DROP TABLE `filterrecord_new`;"));
+
+			// create indexes
+			exec(const_cast<char *>("CREATE INDEX `filterrecord_hashid_index` ON `filterrecord` (`hash_id`);"));
+			exec(const_cast<char *>("CREATE INDEX `imagerecord_hashid_index` ON `imagerecord` (`hash_id`);"));
+
 			break;
 
 		default:
+			LOG4CPLUS_ERROR(logger,"Unknown schema version " << dbVersion << ", aborting...");
+			throw std::runtime_error("Unknown schema version");
 			break;
 	}
 
@@ -455,4 +510,3 @@ void Database::setUserSchemaVersion(int version) {
 
 	Database::exec(command.str().c_str());
 }
-
