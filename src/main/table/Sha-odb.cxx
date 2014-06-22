fcfd09d01461e320b8b1a2d0807552f79ba62f6a
dozedoff <dozedoffagain@gmail.com>
2014-06-22 19:07:20 +0200

    Updated generated Sha ORM files

--- src/main/table/Sha-odb.cxx
+++ src/main/table/Sha-odb.cxx
@@ -19,6 +19,7 @@
 #include <odb/sqlite/simple-object-statements.hxx>
 #include <odb/sqlite/container-statements.hxx>
 #include <odb/sqlite/exceptions.hxx>
+#include <odb/sqlite/prepared-query.hxx>
 #include <odb/sqlite/simple-object-result.hxx>
 
 namespace odb
@@ -641,6 +642,99 @@ namespace odb
       q.parameters_binding ());
 
     return st.execute ();
+  }
+
+  odb::details::shared_ptr<prepared_query_impl>
+  access::object_traits_impl< ::imageHasher::db::table::Sha, id_sqlite >::
+  prepare_query (connection& c, const char* n, const query_base_type& q)
+  {
+    using namespace sqlite;
+    using odb::details::shared;
+    using odb::details::shared_ptr;
+
+    sqlite::connection& conn (
+      static_cast<sqlite::connection&> (c));
+
+    statements_type& sts (
+      conn.statement_cache ().find_object<object_type> ());
+
+    image_type& im (sts.image ());
+    binding& imb (sts.select_image_binding ());
+
+    if (im.version != sts.select_image_version () ||
+        imb.version == 0)
+    {
+      bind (imb.bind, im, statement_select);
+      sts.select_image_version (im.version);
+      imb.version++;
+    }
+
+    std::string text (query_statement);
+    if (!q.empty ())
+    {
+      text += " ";
+      text += q.clause ();
+    }
+
+    shared_ptr<sqlite::prepared_query_impl> r (
+      new (shared) sqlite::prepared_query_impl (conn));
+    r->name = n;
+    r->execute = &execute_query;
+    r->query = q;
+    r->stmt.reset (
+      new (shared) select_statement (
+        conn,
+        text,
+        false,
+        true,
+        r->query.parameters_binding (),
+        imb));
+
+    return r;
+  }
+
+  odb::details::shared_ptr<result_impl>
+  access::object_traits_impl< ::imageHasher::db::table::Sha, id_sqlite >::
+  execute_query (prepared_query_impl& q)
+  {
+    using namespace sqlite;
+    using odb::details::shared;
+    using odb::details::shared_ptr;
+
+    sqlite::prepared_query_impl& pq (
+      static_cast<sqlite::prepared_query_impl&> (q));
+    shared_ptr<select_statement> st (
+      odb::details::inc_ref (
+        static_cast<select_statement*> (pq.stmt.get ())));
+
+    sqlite::connection& conn (
+      sqlite::transaction::current ().connection ());
+
+    // The connection used by the current transaction and the
+    // one used to prepare this statement must be the same.
+    //
+    assert (&conn == &st->connection ());
+
+    statements_type& sts (
+      conn.statement_cache ().find_object<object_type> ());
+
+    image_type& im (sts.image ());
+    binding& imb (sts.select_image_binding ());
+
+    if (im.version != sts.select_image_version () ||
+        imb.version == 0)
+    {
+      bind (imb.bind, im, statement_select);
+      sts.select_image_version (im.version);
+      imb.version++;
+    }
+
+    pq.query.init_parameters ();
+    st->execute ();
+
+    return shared_ptr<result_impl> (
+      new (shared) sqlite::object_result_impl<object_type> (
+        pq.query, st, sts, 0));
   }
 }
 
