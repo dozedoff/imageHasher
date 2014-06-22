b6f7a7db23556c4998a62ef89e9cec3dad423a70
dozedoff <dozedoffagain@gmail.com>
2014-06-22 19:51:04 +0200

    Created storage class for bad file records

---
+++ src/main/table/BadFileRecord.hpp
@@ -0,0 +1,51 @@
+/*
+ * imageHasher - C++ version of the Hashing module from SimilarImage
+ * Copyright (C) 2014  Nicholas Wright
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file BadFileRecord.hpp
+ *
+ * @date 22 Jun 2014
+ * @author Nicholas Wright
+ */
+
+#ifndef BADFILERECORD_HPP_
+#define BADFILERECORD_HPP_
+
+#include <odb/core.hxx>
+
+namespace imageHasher {
+namespace db {
+namespace table {
+
+#pragma db object table("badfilerecord")
+class BadFileRecord {
+public:
+	BadFileRecord();
+
+private:
+	#pragma db id
+	std::string path;
+
+	friend class odb::access;
+};
+
+} /* namespace table */
+} /* namespace db */
+} /* namespace imageHasher */
+
+#endif /* BADFILERECORD_HPP_ */
