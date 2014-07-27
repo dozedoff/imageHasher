b77d60a62b7f83fef11b12983a30eb75c09a5a6f
dozedoff <dozedoffagain@gmail.com>
2014-07-27 16:30:23 +0200

    Created storage class for Settings

---
+++ src/main/table/Settings.hpp
@@ -0,0 +1,49 @@
+/*
+ * imageHasher - C++ version of the Hashing module from SimilarImage
+ * Copyright (C) 2014  Nicholas Wright
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file Settings.hpp
+ *
+ * @date 27 Jul 2014
+ * @author Nicholas Wright
+ */
+
+#ifndef SETTINGS_HPP_
+#define SETTINGS_HPP_
+
+#include <odb/core.hxx>
+
+namespace db {
+namespace table {
+
+class Settings {
+public:
+	Settings();
+
+private:
+	#pragma db id
+	std::string key;
+
+	std::string value;
+
+	friend class odb::access;
+};
+
+} /* namespace table */
+} /* namespace db */
+#endif /* SETTINGS_HPP_ */
