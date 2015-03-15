8a11feef8047049a7ff4f4c42c47b4801dea180e
Nicholas Wright <dozedoffagain@gmail.com>
2015-03-15 18:55:32 +0100

    Re-worded hashworker summary

--- src/main/HashWorker.cpp
+++ src/main/HashWorker.cpp
@@ -47,7 +47,7 @@ void HashWorker::start() {
 	tg.join_all();
 	db.shutdown();
 	LOG4CPLUS_INFO(logger, "All worker thread(s) have terminated");
-	LOG4CPLUS_INFO(logger, "Hashed " << db.getRecordsWritten() << " of " << totalNumOfFiles << " images, sha-lookup:"<< db.get_sha_found() << ", skipped " << skipped_files << ", invalid: " << db.get_invalid_files());
+	LOG4CPLUS_INFO(logger, "Hashed " << db.getRecordsWritten() << " of " << totalNumOfFiles << " images, sha lookup hits:"<< db.get_sha_found() << ", skipped " << skipped_files << ", invalid: " << db.get_invalid_files());
 }
 
 void HashWorker::clear() {
