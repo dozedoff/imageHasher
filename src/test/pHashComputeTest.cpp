741874b40bb0960d01333c82d83af88ba86c7ee3
Nicholas Wright <dozedoffagain@gmail.com>
2015-03-19 18:42:15 +0100

    Added pull socket to pHashComputeTest

--- src/test/pHashComputeTest.cpp
+++ src/test/pHashComputeTest.cpp
@@ -13,13 +13,26 @@ class pHashComputeTest : public :: testing::Test {
 protected:
 	imageHasher::pHashCompute *phc;
 
+	zmq::context_t *context;
+	zmq::socket_t *pull_socket;
+
 	pHashComputeTest() {
+		context = new zmq::context_t(1);
+		pull_socket = new zmq::socket_t(*context, ZMQ_PULL);
+		pull_socket->bind("tcp://*:4444");
+
 		phc = NULL;
 		phc = new imageHasher::pHashCompute(4,5555,"tcp://127.0.0.1:4444");
 	}
 
 	~pHashComputeTest() {
 		delete(phc);
+
+		pull_socket->close();
+		context->close();
+
+		delete(pull_socket);
+		delete(context);
 	}
 };
 
